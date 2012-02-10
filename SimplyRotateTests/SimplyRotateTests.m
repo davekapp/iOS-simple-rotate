f291ee04f3a842182573d20e6d3c05b0e8f3e908
David Kapp <davidkapp@coshx.com>
2012-02-09 18:08:05 -0700

    Initial Commit

---
+++ SimplyRotateTests/SimplyRotateTests.m
@@ -0,0 +1,32 @@
+//
+//  SimplyRotateTests.m
+//  SimplyRotateTests
+//
+//  Created by David Kapp on 2/9/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "SimplyRotateTests.h"
+
+@implementation SimplyRotateTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in SimplyRotateTests");
+}
+
+@end
